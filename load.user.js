20baa6c943bdf9ec7c4599aa4e27183e90f546f8
Kalon <kaloncpu57@gmail.com>
2016-04-25 23:27:21 -0600

    Update and rename user.js to load.user.js

--- load.user.js
+++ load.user.js
@@ -2,7 +2,7 @@
 // @name         Twitch Color Cycler
 // @namespace    https://twitch.tv/kaloncpu57
 // @version      1.1
-// @updateURL    http://kaloncpu57.github.io/twitch-scripts/user.js
+// @updateURL    http://kaloncpu57.github.io/twitch-scripts/load.user.js
 // @description  Change your Twitch.tv username color in chat every time you post.
 // @author       kaloncpu57
 // @match        http*://www.twitch.tv/*
