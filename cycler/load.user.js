141ad10a5afeff8934b83a79b6c72d45a7cc6bd1
Kalon <kaloncpu57@gmail.com>
2016-05-19 04:17:51 -0600

    Fix for Greasemonkey/Firefox users
    Fixed non-secure updateURL and "http*" for Greasemonkey

--- cycler/load.user.js
+++ cycler/load.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Twitch Color Cycler
 // @namespace    https://twitch.tv/kaloncpu57
-// @version      1.1
-// @updateURL    http://kaloncpu57.github.io/twitch-scripts/cycler/load.user.js
+// @version      1.2
+// @updateURL    https://kaloncpu57.github.io/twitch-scripts/cycler/load.user.js
 // @description  Change your Twitch.tv username color in chat every time you post.
 // @author       kaloncpu57
-// @match        http*://www.twitch.tv/*
+// @match        https://www.twitch.tv/*
 // @grant        none
 // ==/UserScript==
 
