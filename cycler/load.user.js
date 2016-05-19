fc1ea3916da801f0f26795696c3a115973ef54da
Kalon <kaloncpu57@gmail.com>
2016-05-19 04:31:00 -0600

    Firefox load fix
    Changed when the script loads in order to work properly on Firefox

--- cycler/load.user.js
+++ cycler/load.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Color Cycler
 // @namespace    https://twitch.tv/kaloncpu57
-// @version      1.2
+// @version      1.2.1
 // @updateURL    https://kaloncpu57.github.io/twitch-scripts/cycler/load.user.js
 // @description  Change your Twitch.tv username color in chat every time you post.
 // @author       kaloncpu57
